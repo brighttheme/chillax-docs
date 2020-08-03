5a51d007b6feb5bbc8c46257534473436f811be2
Salim Hossain <salimhossain@gmail.com>
2020-08-03 09:05:26 +0600

    init

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -53,7 +53,7 @@ function initNav() {
 
 function initSearch() {
   var request = new XMLHttpRequest();
-  request.open('GET', 'http://localhost:4000/chillax-docs/assets/js/search-data.json', true);
+  request.open('GET', '/chillax-docs/assets/js/search-data.json', true);
 
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
