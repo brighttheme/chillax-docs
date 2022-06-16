867950116f374afab8e4a64e9907591ec26542d2
Salim Hossain <salimhossain@gmail.com>
2022-06-16 15:20:27 +0600

    init

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -430,7 +430,7 @@ jtd.getTheme = function() {
 
 jtd.setTheme = function(theme) {
   var cssFile = document.querySelector('[rel="stylesheet"]');
-  cssFile.setAttribute('href', '/chillax-docs/assets/css/just-the-docs-' + theme + '.css');
+  cssFile.setAttribute('href', 'https://meshtheme.github.io/chillax-docs/assets/css/just-the-docs-' + theme + '.css');
 }
 
 // Document ready
